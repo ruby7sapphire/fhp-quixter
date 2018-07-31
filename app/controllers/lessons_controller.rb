0daaa966d2ec86e20fdd762b8d0886e14dd1f503
ruby7sapphire <wewillmeet@icloud.com>
2018-07-31 01:22:39 -0700

    Lesson 28: Permissions

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,11 +1,25 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_enrollment_in_current_course
+
   def show
   end
 
   private
 
+  def require_enrollment_in_current_course
+    if !current_user.enrolled_in?(current_course)
+      redirect_to course_path(current_course), alert: 'You need to enroll in the lesson to access it.'
+    end
+  end
+
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
+  end
+
+  helper_method :current_course
+  def current_course
+    @current_course ||= current_lesson.section.course
   end
 end
