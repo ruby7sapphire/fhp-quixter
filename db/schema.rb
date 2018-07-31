db1a90ebeefe31b1a7936360ffe7865689520ddb
ruby7sapphire <wewillmeet@icloud.com>
2018-07-30 17:35:41 -0700

    Lesson 24: Video Uploading

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_21_004123) do
+ActiveRecord::Schema.define(version: 2018_07_31_002037) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2018_07_21_004123) do
     t.integer "section_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "video"
     t.index ["section_id"], name: "index_lessons_on_section_id"
   end
 
